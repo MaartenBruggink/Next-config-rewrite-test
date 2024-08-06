1a700053896fd9506db7b67d36feb57aee69df6b
Maarten Bruggink <maarten@obelism.studio>
2024-08-06 21:20:31 +0200

    🌎 - Redirect default locale

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,24 @@
+import { DEFAULT_URL_LOCALE } from "./src/config/locales.mjs";
 import { generateDefaultLocaleRewrites } from "./src/functions/generateDefaultLocaleRewrites.mjs";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
     return generateDefaultLocaleRewrites();
+  },
+  async redirects() {
+    return [
+      {
+        source: `/${DEFAULT_URL_LOCALE}`,
+        destination: `/`,
+        permanent: true,
+      },
+      {
+        source: `/${DEFAULT_URL_LOCALE}/:match*`,
+        destination: `/:match*`,
+        permanent: true,
+      },
+    ];
   },
 };
 
